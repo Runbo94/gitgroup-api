120934c0adb7f93b5a4b6c05ad7e98b1638c8d6a
Runbo Zhao <rzhao03@syr.edu>
2018-12-03 18:48:29 -0500

    build fix.

--- lib/server.ts
+++ lib/server.ts
@@ -1,6 +1,6 @@
 // lib/server.ts
 import app from "./app";
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log("Express server listening on port " + PORT);
